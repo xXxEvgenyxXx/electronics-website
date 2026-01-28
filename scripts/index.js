89ef9dc8dc219be80ddb657fd0d680ae3d33be10
Evgeny Simonov <jdsimonov2@gmail.com>
2026-01-28 21:52:37 +0300

    fix: remove burger animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,3 @@
-// Бургер-меню
-const menuToggle = document.getElementById('menuToggle');
-const mainNav = document.getElementById('mainNav');
-
-menuToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('active');
-    
-    // Анимация бургера
-    const bars = menuToggle.querySelectorAll('.bar');
-    bars[0].classList.toggle('bar-open-top');
-    bars[1].classList.toggle('bar-open-middle');
-    bars[2].classList.toggle('bar-open-bottom');
-});
-
-// Закрытие меню при клике на ссылку
-document.querySelectorAll('#mainNav a').forEach(link => {
-    link.addEventListener('click', () => {
-        mainNav.classList.remove('active');
-        menuToggle.querySelectorAll('.bar').forEach(bar => {
-            bar.className = 'bar';
-        });
-    });
-});
-
 // Меню "Прочее"
 const moreOptionsButton = document.querySelector('.more-button');
 const moreOptions = document.querySelector('.more-options-wrapper');
